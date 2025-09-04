13b14124896ddb17fbfd206d372003775d68d274
drahenios <xlegaspij@protonmail.ch>
2025-09-04 14:29:05 +0200

    Corrected finder.sh

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -5,7 +5,7 @@
 # and number of lines containing a string.
 
 # Check for two arguments
-if [$# -ne 2]; then
+if [ "$#" -ne 2 ]; then
     echo "Error: Two arguments are required."
     echo "Usage: $0 <filesdir> <searchstr>"
     exit 1
@@ -14,19 +14,8 @@ fi
 filesdir=$1
 searchstr=$2
 
-# Check if any of the arguments is an empty string
-if [-z filesdir]; then
-    echo "Error: <filesdir> must be specified." >&2
-    exit 1
-fi
-
-if [-z searchstr]; then
-    echo "Error: <searchstr> must be specified." >&2
-    exit 1
-fi
-
 # Check if filesdir is a valid directory
-if [-d filesdir]; then
+if [ ! -d "$filesdir" ]; then
     echo "Error: $filesdir is not a directory." >&2
     exit 1
 fi
